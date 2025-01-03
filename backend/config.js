485c9a312af34bed92cf774fd49083524ffc2b18
Michael <119465676+AcidReef@users.noreply.github.com>
2025-01-03 18:52:17 +0100

    Ukończony projekt Fullstack
    Zawiera nierozwiązany błąd polegający na niewyświetleniu danych z widoków informacji oraz edycji

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://root:5dt2zbgLN957LB7r@bookstore.shypx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore';
---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose, { mongo } from "mongoose";
+import { Book } from "./models/bookModel.js";
+import  booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(200).send('Hello World');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
